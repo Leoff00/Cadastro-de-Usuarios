96fd98d1b9c9c086ed8291df4ccdb20bc19aa9dd
Leoff00 <leof0180@gmail.com>
2020-09-28 22:51:18 -0300

    change in connection

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_PRODUCTION == "production") {
 
-    module.exports = {mongoURI: "mongodb+srv://Leonardoferreira:sk8forever@cad1.xztca.mongodb.net/Cad1?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://Leonardoferreira:sk8forever@cad1.xztca.mongodb.net/CrudApp?retryWrites=true&w=majority"}
 
 } else {
     module.exports = {mongoURI: "mongodb://localhost/blogApp"}
