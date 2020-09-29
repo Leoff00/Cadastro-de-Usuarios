56dd785f7a5825240ac5688f09f4b76bbeb1ed4a
Leoff00 <leof0180@gmail.com>
2020-09-29 00:11:19 -0300

    make it better

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {mongoURI: "mongodb+srv://Leonardoferreira:sk8forever@cad1.xztca.mongodb.net/CrudApp?retryWrites=true&w=majority"}
 
-module.exports = {mongoURI2: "mongodb://localhost/blogApp"}
+//module.exports = {mongoURI2: "mongodb://localhost/blogApp"}
 
